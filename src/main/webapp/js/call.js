901b57a1bdcf85417d8af86e084da7652b4fc0b7
Sacha Nacar <sacha.slushy@gmail.com>
2016-02-04 11:04:08 +0000

    Update call.js

--- src/main/webapp/js/call.js
+++ src/main/webapp/js/call.js
@@ -1,51 +1,6 @@
-// Register callbacks to desired call events
-var eventHandlers = {
-
-    'progress':   function(e){
-                        $("#dial").html('<i id="phone-dial" class="icon-phone"></i> Calling');
-                        $( "#phone-dial" ).toggleClass( "spin", true );
-                        $( "#dial" ).toggleClass( "disabled", true );
-                        $( "#dial" ).attr('disabled', 'disabled');
-                        $( "#hangup" ).toggleClass( "disabled", false );
-                        $( "#mute" ).toggleClass( "disabled", false );
-                    },
-    'failed':     function(e){
-                        $( "#dial" ).html('Failed to Connect');
-                        $( "#dial" ).toggleClass( "incall", true );
-                        $( "#dial" ).attr('disabled', 'disabled');
-                        $( "#hangup" ).toggleClass( "disabled", true );
-                        $( "#mute" ).toggleClass( "disabled", true );
-                        timeout = setTimeout(function() {
-                            $("#dial").html('<i class="icon-phone"></i>');
-                            $( "#dial" ).removeAttr('disabled');
-                            $( "#dial" ).toggleClass( "incall", false );
-                            $( "#dial" ).toggleClass( "disabled", false );
-                        }, 2500);
-                    },
-    'started':    function(e){
-                        // Start stopwatch
-                        stopwatch.startStop();
-                        $( "#dial" ).toggleClass( "incall", true );
-                        $( "#dial" ).attr('disabled', 'disabled');
-                    },
-    'ended':      function(e){
-                        // Stop stopwatch
-                        stopwatch.startStop();
-
-                        $("#dial").html('Call Ended');
-                        $( "#hangup" ).toggleClass( "disabled", true );
-                        $( "#mute" ).toggleClass( "disabled", true );
-                        timeout = setTimeout(function() {
-                                $( "#dial" ).toggleClass( "incall", false );
-                                $( "#dial" ).toggleClass( "disabled", false );
-                                $( "#dial" ).removeAttr('disabled');
-                            }, 2000);
-                        timeout = setTimeout(function() {
-                                $("#dial").html('<i class="icon-phone"></i>');
-                            }, 2500);
-                    }
-};
-
+//Add your webRTC credentials
+var username ="";
+var secret = "";
 /** Make call Event **/
 var callTimer;
 function makeCall(){
@@ -76,15 +31,72 @@ function init(){
     //voxbone.WebRTC.preferedPop = 'TEST';
 
     // set custom event handlers
-    voxbone.WebRTC.customEventHandler = eventHandlers;
-
+      voxbone.WebRTC.customEventHandler.progress = function(e) {
+		$("#dial").html('<i id="phone-dial" class="icon-phone"></i> Calling');
+        $( "#phone-dial" ).toggleClass( "spin", true );
+        $( "#dial" ).toggleClass( "disabled", true );
+        $( "#dial" ).attr('disabled', 'disabled');
+        $( "#hangup" ).toggleClass( "disabled", false );
+        $( "#mute" ).toggleClass( "disabled", false );
+	  };
+	  voxbone.WebRTC.customEventHandler.getUserMediaFailed = function(e) {
+		$( "#dial" ).html('Failed to Connect');
+        $( "#dial" ).toggleClass( "incall", true );
+        $( "#dial" ).attr('disabled', 'disabled');
+        $( "#hangup" ).toggleClass( "disabled", true );
+        $( "#mute" ).toggleClass( "disabled", true );
+        timeout = setTimeout(function() {
+            $("#dial").html('<i class="icon-phone"></i>');
+            $( "#dial" ).removeAttr('disabled');
+            $( "#dial" ).toggleClass( "incall", false );
+            $( "#dial" ).toggleClass( "disabled", false );
+        }, 2500);
+	  };
+	  };
+  	  voxbone.WebRTC.customEventHandler.failed = function(e) {
+		$( "#dial" ).html('Failed to Connect');
+        $( "#dial" ).toggleClass( "incall", true );
+        $( "#dial" ).attr('disabled', 'disabled');
+        $( "#hangup" ).toggleClass( "disabled", true );
+        $( "#mute" ).toggleClass( "disabled", true );
+        timeout = setTimeout(function() {
+            $("#dial").html('<i class="icon-phone"></i>');
+            $( "#dial" ).removeAttr('disabled');
+            $( "#dial" ).toggleClass( "incall", false );
+            $( "#dial" ).toggleClass( "disabled", false );
+        }, 2500);
+	  };
+      voxbone.WebRTC.customEventHandler.accepted = function(e) {
+		 // Start stopwatch
+        stopwatch.startStop();
+        $( "#dial" ).toggleClass( "incall", true );
+        $( "#dial" ).attr('disabled', 'disabled');
+	  };
+      voxbone.WebRTC.customEventHandler.ended = function(e) {
+		 // Stop stopwatch
+        stopwatch.startStop();
+        $("#dial").html('Call Ended');
+        $( "#hangup" ).toggleClass( "disabled", true );
+        $( "#mute" ).toggleClass( "disabled", true );
+        timeout = setTimeout(function() {
+                $( "#dial" ).toggleClass( "incall", false );
+                $( "#dial" ).toggleClass( "disabled", false );
+                $( "#dial" ).removeAttr('disabled');
+            }, 2000);
+        timeout = setTimeout(function() {
+                $("#dial").html('<i class="icon-phone"></i>');
+            }, 2500);
+        }
+	  };
+	
     //Set the caller-id, domain name gets automatically stripped off
     //Note that It must be a valid sip uri.
     //Default value is: voxrtc@voxbone.com
     //voxbone.WebRTC.configuration.uri = "caller-id@voxbone.com";
 
     //Bootstrap Voxbone WebRTC javascript object
-    voxbone.WebRTC.init(voxrtc_config);
+    //voxbone.WebRTC.init(voxrtc_config);
+    voxbone.WebRTC.basicAuthInit(username, secret);
 
     /** Parse through Email footer url for Number value to call **/
     function getURLParameter(name) {
